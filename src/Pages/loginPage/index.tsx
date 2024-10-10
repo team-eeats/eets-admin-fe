0707b0180b558d5c8398d0d835db96e7b93df85a
heedda <heedda.kollim@gmail.com>
2024-10-10 19:22:01 +0900

    ♻️ :: login page 리팩

--- src/Pages/loginPage/index.tsx
+++ src/Pages/loginPage/index.tsx
@@ -4,10 +4,12 @@ import Input from "../../Components/Input/Input";
 import Button from "../../Components/Button";
 import { useEffect, useState } from "react";
 import useInputStore from "../../store/useInputStore";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const [buttonActive, setButtonActive] = useState<boolean>(false);
   const { inputs } = useInputStore();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (inputs["loginText"] && inputs["loginPassword"]) setButtonActive(true);
@@ -15,21 +17,21 @@ const LoginPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [inputs["loginText"], inputs["loginPassword"]]);
 
+  const clickHandle = () => {
+    navigate("/main");
+  };
+
   return (
     <S.LoginPageContainer>
       <div style={{ gap: "20px" }}>
         <Font text="eeats! 로그인 하기" kind="Heading1" color="black" />
-        <Font
-          text="스퀘어 계정으로 로그인해주세요"
-          kind="label2"
-          color="black"
-        />
+        <Font text="어드민 계정입니다" kind="label2" color="gray500" />
       </div>
       <div style={{ gap: "52px" }}>
         <Input id="loginText" type="text" placeholder="아이디 입력" />
         <Input id="loginPassword" type="password" placeholder="비밀번호 입력" />
       </div>
-      <Button text="로그인" activate={buttonActive} onClick={() => {}} />
+      <Button text="로그인" activate={buttonActive} onClick={clickHandle} />
     </S.LoginPageContainer>
   );
 };
